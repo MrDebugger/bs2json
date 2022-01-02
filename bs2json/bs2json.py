f5d560d6e2afeb2129f05fcc0ed7468549c81045
Ijaz Ur Rahim ( The Alien ) <ijazkhan095@gmail.com>
2022-01-03 03:18:02 +0500

    version 0.0.2
    Removed many bugs and added changing label support

--- bs2json/bs2json.py
+++ bs2json/bs2json.py
@@ -1,7 +1,17 @@
 from bs4 import ResultSet,element as Element
 
 class bs2json:
-	def convert(self,element,json={}):
+	def __init__(self):
+		self.__labels = {}
+		self.labels(attributes='attributes',text='text')
+
+	def labels(self,**kwargs):
+		for k,w in kwargs.items():
+			self.__labels[k] = w
+
+	def convert(self,element,json=None):
+		if not json:
+			json ={}
 		if not type(json) == dict:
 			raise TypeError(f"convertAll(x,y) `y` argument must be a {dict.__name__} not {type(json).__name__}")
 		if type(element) == Element.Tag:
@@ -10,37 +20,55 @@ class bs2json:
 		else:
 			raise TypeError(f"convert(x,y) `x` argument must be a {Element.__name__} not {type(element).__name__}")
 
-	def convertAll(self,elements,L=None):
-		List = L if L is not None else []
+	def convertAll(self,elements,List=None,join=False):
+		if not List:
+			List = []
 		if not type(List) == list:
-			raise TypeError(f"convertAll(x,y) `y` argument must be a {list.__name__} not {type(List).__name__}")
+			raise TypeError(f"convertAll(x,y) `y` argument must be a {list} not {type(List).__name__}")
 		if type(elements) == ResultSet:
+			json = {}
 			for element in elements:
-				List.append(self.toJson(element))
-			return List
+				if join:
+					if json.get(element.name):
+						json[element.name].append(self.toJson(element))
+					else:
+						json[element.name] = [self.toJson(element)]
+				else:
+					List.append({element.name:self.toJson(element)})
+			return List+[json] if join else List  
 		else:
 			raise TypeError(f"convertAll(x,y) `x` argument must be a {ResultSet.__name__} not {type(elements).__name__}")
 
-	def toJson(self,elem=None,json={}):
-		if type(elem) == Element.Tag:
-			json[elem.name] = {'attributes':elem.attrs}
-			json[elem.name] = self.toJson(elem.contents,json[elem.name])
-			return json[elem.name]
-		elif len(elem):
+	def toJson(self,element=None):
+		json = {}
+		if type(element) == Element.Tag:
+			json[element.name] = {}
+			if element.contents:
+				json[element.name] = self.toJson(element.contents)
+			elif element.string or element.text:
+				json[element.name] = {self.__labels['text']:element.text or element.string}
+			json[element.name][self.__labels['attributes']]= element.attrs
+			return json[element.name]
+		elif type(element)==list:
 			elements = []
 			text = ''
-			for el in elem:
-				if type(el) == Element.Tag:
-					value = self.toJson(el,json)
-					elements.append({el.name:value})
-				else:
-					text += el.strip()
-			json['text'] = text
-			keys = [key for el in elements for key in el.keys()]
+			for elem in element:
+				if type(elem) == Element.Tag:
+					value = self.toJson(elem)
+					elements.append({elem.name:value})
+				elif elem.strip():
+					text += elem.strip()
+			if text:
+				json[self.__labels['text']] = text
+			keys = [
+						key 
+						for elem in elements 
+						for key in elem.keys()
+					]
 			for key in set(keys):
 				values = []
-				for el in elements:
-					for Key,value in el.items():
+				for elem in elements:
+					for Key,value in elem.items():
 						if Key == key:
 							if value:
 								values.append(value)
